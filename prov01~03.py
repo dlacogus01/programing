50686791f68a847da919302acb81c24b35e3731e
dlacogus01 <65270396+dlacogus01@users.noreply.github.com>
2020-05-27 15:54:08 +0900

    Add files via upload

---
+++ prov01~03.py
@@ -0,0 +1,10 @@
+movie_rank = ["닥터 스트레인지","스플릿","럭키"]
+movie_rank_append("배트맨")
+print(movie_rank)
+
+
+movie_rank = ['닥터 스트레인지', '스플릿', '럭키', '베트멘']
+movie_rank.insert(1, "배트맨")
+print(movie_rank)
+
+
