7f2e1adc39256dbaa835c53a2d7b8eebf180984d
dlacogus01 <65270396+dlacogus01@users.noreply.github.com>
2020-06-03 15:55:37 +0900

    Add files via upload

---
+++ if01.py
@@ -0,0 +1,9 @@
+#file name: if01.py
+#if (조건문)에서 조건문이 True이면 if 아래에 있는 명령들이 실행
+
+a =int(input("점수 입력"))
+
+if a>=80:
+    print("잘했습니다") 
+
+    
---
+++ if03.py
@@ -0,0 +1,13 @@
+#file name: if01.py
+#if (조건문)에서 조건문이 True이면 if 아래에 있는 명령들이 실행
+#                         Flase이면 else 아래에 있는 명령들이 실행
+
+a =int(input("점수 입력"))
+
+if a>=80:
+    print("훌륭합니다.")
+
+else:
+    print("노력이 필요합니다.")
+
+print("수고하셨습니다.")
